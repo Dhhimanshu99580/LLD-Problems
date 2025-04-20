a49bb878500b0001f83f6a59bf5cde3bab9dce0f
himanshu.ranjan <himanshu99580@gmail.com>
2025-04-20 23:00:42 +0530

    tictactoe

--- tictactoe/Board.java
+++ tictactoe/Board.java
@@ -15,7 +15,16 @@ public class Board {
         }
     }
     public void showAvailableSlots() {
-        System.out.println(board);
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (board[i][j] == null) {
+                    System.out.print("- ");
+                } else {
+                    System.out.print(board[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
     }
     public boolean isValidMove(int row, int col) {
         if(row>=0 && row<size && col>-0 && col<size) {
@@ -31,5 +40,67 @@ public class Board {
     public boolean insertMove(int row, int col, Player player) {
         board[row][col] = player.getMoveType();
         return true;
+    }
+
+    public boolean checkForWin(Player player) {
+        if(checkForRow(player.getMoveType())||checkForColumn(player.getMoveType())||checkForDiagonal(player.getMoveType())) {
+            System.out.println(player.name + "has won the match with moveType as"+ player.getMoveType());
+            return true;
+        }
+        return false;
+    }
+    private boolean checkForRow(MoveType moveType) {
+        int count=0;
+        for(int i=0;i<size;i++) {
+            if(board[i][0]==moveType) {
+                 count=1;
+                for(int j=1;j<size;j++) {
+                    if(board[i][j]==moveType) {
+                        count++;
+                    }
+                }
+            }
+            if(count==size) return true;
+        }
+        return false;
+    }
+
+    private boolean checkForColumn(MoveType moveType) {
+        int count=0;
+        for(int i=0;i<size;i++) {
+            if (board[0][i] == moveType) {
+                count = 1;
+                for (int j = 1; j < size; j++) {
+                    if (board[j][i] == moveType) {
+                        count++;
+                    }
+                }
+            }
+            if (count == size) return true;
+        }
+        return false;
+    }
+
+    private boolean checkForDiagonal(MoveType moveType){
+        int count;
+        if(board[0][0]==moveType) {
+            count=1;
+            for(int i=1;i<size;i++) {
+                if(board[i][i]==moveType){
+                    count++;
+                }
+            }
+            if(count==size) return true;
+        }
+        if(board[size-1][size-1]==moveType) {
+            count=1;
+            for(int i=1;i<size;i++) {
+                if(board[size-1][i]==moveType) {
+                    count++;
+                }
+            }
+            if(count==size) return true;
+        }
+        return false;
     }
 }
--- tictactoe/Game.java
+++ tictactoe/Game.java
@@ -9,18 +9,38 @@ public class Game {
     public static void main(String[] argss) {
         List<Player> player = new ArrayList<>();
         player.add(new Player("Himanshu",new MoveO()));
-        player.add(new Player("Ravi",new MoveX()));
+        player.add(new Player("Mansi",new MoveX()));
         Board board = new Board(5);
         System.out.println("Lets start with Himanshu");
         System.out.println("Show Available slots");
-        board.showAvailableSlots();
-        System.out.println("Enter the row and column where you want to insert");
-        Scanner sc = new Scanner(System.in);
-        int row = sc.nextInt();
-        int col = sc.nextInt();
-        if(board.isValidMove(row,col)) {
-            board.insertMove(row,col,player.get(0));
+        int count=0;
+        for(int i=0;i<5*5;i++) {
+            count++;
+            boolean mansi = false;
+            if(i%2==0) {
+                System.out.println("Himanshus turn");
+            } else {
+                mansi = true;
+                System.out.println("Mansi's turn");
+            }
+            board.showAvailableSlots();
+            System.out.println("Enter the row and column where you want to insert");
+            Scanner sc = new Scanner(System.in);
+            int row = sc.nextInt();
+            int col = sc.nextInt();
+            if(mansi&&board.isValidMove(row,col)) {
+                board.insertMove(row,col,player.get(1));
+            }
+            if(board.isValidMove(row,col)) {
+                board.insertMove(row,col,player.get(0));
+            }
+            if(count>=5+5-1) {
+               boolean checkForHimanshu =  board.checkForWin(player.get(0));
+               boolean checkForMansi =  board.checkForWin(player.get(1));
+               if(checkForMansi||checkForHimanshu) break;
+            }
         }
+
 
         //check validy for players to win
         //if condition is met then return true;
